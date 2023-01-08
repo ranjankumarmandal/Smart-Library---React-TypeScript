e51a137c6da07acabf09614126565c297c31f102
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-01-08 12:33:04 +0530

    updated title, navbar and added component NavBar.tsx to root component App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to the Full Stack Course</h1>
-    </div>
-  );
+  return <NavBar />;
 }
 
 export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className='navbar navbar-expand-lg navbar-dark main-color py-3'>
       <div className='container-fluid'>
-        <span className='navbar-brand'>SLS RKM</span>
+        <span className='navbar-brand'>Ranjan Library</span>
         <button
           className='navbar-toggler'
           type='button'
@@ -31,7 +31,7 @@ const NavBar = () => {
           </ul>
           <ul className='navbar-nav ms-auto'>
             <li className='nav-item m-1'>
-              <a href='#' type='button' className='btn btn-otline-light'>
+              <a href='#' type='button' className='btn btn-outline-light'>
                 Sign In
               </a>
             </li>
