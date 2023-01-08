e00099fcd073c926d34e377365e4b0f4a63f40f3
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-01-08 12:29:39 +0530

    added logged out version of navbar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const NavBar = () => {
-    return(
-
-    );
-}
+  return (
+    <div className='navbar navbar-expand-lg navbar-dark main-color py-3'>
+      <div className='container-fluid'>
+        <span className='navbar-brand'>SLS RKM</span>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNavDropdown'
+          aria-controls='navbarNavDropdown'
+          aria-expanded='false'
+          aria-label='Toggle Navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <a href='#' className='nav-link'>
+                Home
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a href='#' className='nav-link'>
+                Search Books
+              </a>
+            </li>
+          </ul>
+          <ul className='navbar-nav ms-auto'>
+            <li className='nav-item m-1'>
+              <a href='#' type='button' className='btn btn-otline-light'>
+                Sign In
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
