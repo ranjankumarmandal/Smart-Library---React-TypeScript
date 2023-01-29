c5ff37835427de064f60fb97ef5290207c9ceb5b
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-01-29 17:28:49 +0530

    added header, paragraph and button for jsx in component ExploreTopBooks

--- src/components/Home/ExploreTopBooks.tsx
+++ src/components/Home/ExploreTopBooks.tsx
@@ -3,7 +3,19 @@ import React from "react";
 const ExploreTopBooks = () => {
   return (
     <div className='p-5 mb-4 bg-dark header'>
-      <div className='container-fluid py-5 text-white d-flex justify-content-center align-items-center'></div>
+      <div className='container-fluid py-5 text-white d-flex justify-content-center align-items-center'>
+        <div>
+          <h1 className='display-5 fw-bold'>Find your next adventure</h1>
+          <p className='col-md-8 fs-4'>Where would you like to go next?</p>
+          <a
+            type='button'
+            href='#'
+            className='btn main-color btn-lg text-white'
+          >
+            Explore top books
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
