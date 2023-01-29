6e4d94f99f4c07b6f28a577814889f558a57fa99
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-01-29 17:26:11 +0530

    started working in component ExploreTopBooks.tsx - added parent dev inside jsx

--- src/components/Home/ExploreTopBooks.tsx
+++ src/components/Home/ExploreTopBooks.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const ExploreTopBooks = () => {
-    return ();
-}
+  return (
+    <div className='p-5 mb-4 bg-dark header'>
+      <div className='container-fluid py-5 text-white d-flex justify-content-center align-items-center'></div>
+    </div>
+  );
+};
 
 export default ExploreTopBooks;
