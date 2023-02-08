afd20a3be5f2dc50ea16e726c41ba000d15d38d9
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-02-09 00:04:32 +0530

    import component Carousel to root component App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import ExploreTopBooks from "./components/Home/ExploreTopBooks";
+import Carousel from "./components/Home/Carousel";
 
 function App() {
   return (
