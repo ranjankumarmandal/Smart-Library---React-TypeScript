28f180a30c39634776dfe05aca3315beacc5d476
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-02-08 18:42:36 +0530

    import component ExploreTopBooks to root component App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import ExploreTopBooks from "./components/Home/ExploreTopBooks";
 
 function App() {
   return <NavBar />;
